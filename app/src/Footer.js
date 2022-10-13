fb2e77f12b5d60a8be7c0018a44894a85c1be440
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-10-13 20:35:51 +0000

    added a footer

--- app/src/Footer.js
+++ app/src/Footer.js
@@ -2,24 +2,42 @@ import React from "react";
 
 function FooterTime (){
     return(
-        <>
-        <div className="contianer" id="foot">
-            <div id="time">
-                Hours<br>
-            </br>Monday-Friday 7:00 AM to 8:00 PM
-            <br>
-            </br> Saturday and Sunday 8:00 AM to 9:00 PM
-            </div>
+        // <>
+        // <div className="contianer" id="foot">
+        //     <div id="time">
+        //         Hours<br>
+        //     </br>Monday-Friday 7:00 AM to 8:00 PM
+        //     <br>
+        //     </br> Saturday and Sunday 8:00 AM to 9:00 PM
+        //     </div>
   
-           <div id="address">
-            Address 312 Main Street<br></br>
-            Lexington, Kwntucky 40502
-            </div>
+        //    <div id="address">
+        //     Address 312 Main Street<br></br>
+        //     Lexington, Kwntucky 40502
+        //     </div>
+        // </div>
+        <footer className="navbar navbar-expand-md navbar-dark fixed-bottom bg-warning">
+        <div className="container">
+          <div className="row">
+          <div className="col-6" id="address">Test</div>
+        <div className="col-6" id="time">Test</div>
+          </div>
         </div>
-
+      </footer>
      
-        </>
+        // </>
     )
 }
 
 export default FooterTime
+
+function footerBar () {
+    return (<footer className="navbar navbar-expand-md navbar-dark sticky-bottom bg-warning">
+  <div className="container">
+    <div className="row">
+        <div className="col-6" id="address">Test</div>
+        <div className="col-6" id="time">Test</div>
+    </div>
+  </div>
+</footer>
+)}
